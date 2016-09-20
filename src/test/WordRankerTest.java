5a3b43a078588fb0cee92d5c703bddc563fbb0ce
Andrea Marisio <Andrea Marisio>
2016-09-20 16:25:40 +0200

    Refactoring nome metodo

--- src/test/WordRankerTest.java
+++ src/test/WordRankerTest.java
@@ -6,7 +6,7 @@ import dataStructures.Good;
 
 public class WordRankerTest {
 	@Test
-	private void rank() {
+	private void productTest() {
 		Good g=new Good("webster",
 				"electronic arts the sims 3 movie stuff",
 				"the sims movie stuff permettera ai vostri sims di utilizzare arredi  mobili"
@@ -14,6 +14,7 @@ public class WordRankerTest {
 				+ " il contenuto western e ispirato ai film   spaghetti western   degli anni  60."
 				+ " set in stile saloon con",
 				13.93f);
-		WordRanker ranker.ra
+		//getKeywords "the sims", "the sims movie stuff", "sims"
+		//Product, contain more accessories "arredi  mobili e abbigliamento ispirati ai film"
 	}
 }
